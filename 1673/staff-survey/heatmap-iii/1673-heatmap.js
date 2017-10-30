9c87487f99257a017fbded3b33c39ee58eede158
Cale Tilford <combinekidd@gmail.com>
2017-10-30 22:21:12 +0000

    Use existing svg

--- 1673/staff-survey/heatmap-iii/1673-heatmap.js
+++ 1673/staff-survey/heatmap-iii/1673-heatmap.js
@@ -6,9 +6,9 @@ window.onload = function() {
   var width = 1400 - margin.left - margin.right,
       height = 725 - margin.top - margin.bottom;
 
-  var svg = d3.select("#departmentHeatmap").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  var svg = d3.select("#departmentHeatmap").select("svg")
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
